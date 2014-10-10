97d7691a8991b8012ce679173afc47b2407bdcc5
Brian McKeever <keever@gmail.com>
2014-10-09 19:53:06 -0700

    first stab at an entity component system

---
+++ src/EntityComponentSystem.hs
@@ -0,0 +1,22 @@
+module EntityComponentSystem
+    (Component, Components, createEntity, Entity)
+where
+import qualified Data.HashSet as Set
+import Control.Monad.State.Lazy
+
+data Component = Component String (Maybe Entity -> Entity)
+type Components = Set.HashSet Component
+
+data Entity = Entity Int Components 
+type EntityManagement = State Int
+
+generateEntityId :: EntityManagement Int
+generateEntityId = do
+    n <- get
+    put (n+1)
+    return n
+
+createEntity :: EntityManagement Entity
+createEntity = do
+    serial <- generateEntityId
+    return $ Entity serial Set.empty
